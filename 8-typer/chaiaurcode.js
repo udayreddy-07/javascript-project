67f5c4fd423a94cec134c9f230ad79a2026dd72a
Uday Reddy <107260122+udayreddy-07@users.noreply.github.com>
2024-01-10 21:16:35 +0530

    Delete 8-typer directory

--- 8-typer/chaiaurcode.js
+++
@@ -1,8 +0,0 @@
-const typedTextSpan = document.querySelector('.typed-text');
-const cursor = document.querySelector('.cursor');
-
-const words = ['Love', 'Jhakaas', 'mast', 'dhinchak', 'Weird'];
-
-function type() {}
-
-function erase() {}
